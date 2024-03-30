eea2a23d3258c9174a32d3d1a1b3e5a4c375a3e1
Leo Singer <leo.singer@ligo.org>
2024-03-29 23:28:23 -0400

    Require Numpy >= 1.23 for ufunc data = NULL

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
         Extension(
             "hpx._core",
             define_macros=[
-                ("NPY_TARGET_VERSION", "NPY_1_19_API_VERSION"),
-                ("NPY_NO_DEPRECATED_API", "NPY_1_19_API_VERSION"),
+                ("NPY_TARGET_VERSION", "NPY_1_23_API_VERSION"),
+                ("NPY_NO_DEPRECATED_API", "NPY_1_23_API_VERSION"),
             ],
             extra_compile_args=["-std=c++14"],
             include_dirs=[np.get_include()],

9c4c82e572dc7490f468e50b59575a2d6984f27d
Leo Singer <leo.singer@ligo.org>
2024-05-03 15:54:06 -0400

    Fix type stub

--- lib/hpx/_core.pyi
+++ lib/hpx/_core.pyi
@@ -1,6 +1,6 @@
 import numpy as np
 import numpy.typing as npt
 
-class LinearSphericalInterpolator:
-    def __init__(self, points: npt.ArrayLike, values: npt.ArrayLike): ...
-    def __call__(self, points: npt.ArrayLike) -> npt.NDArray[np.float64]: ...
+def LinearSphericalInterpolator(
+    points: npt.ArrayLike, values: npt.ArrayLike
+) -> np.ufunc: ...
